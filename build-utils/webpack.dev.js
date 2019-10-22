13265b9051b6684c87cc704bf374fe1f51169c28
Tadeu Mendonça <tadeu.mendonca@outlook.com>
2019-10-21 22:08:32 -0300

    Environment variables.

--- build-utils/webpack.dev.js
+++ build-utils/webpack.dev.js
@@ -1,4 +1,13 @@
+const { DefinePlugin } = require('webpack');
+
 module.exports = {
   mode: 'development',
-  devtool: 'eval-source-map'
+  devtool: 'eval-source-map',
+  plugins: [
+    new DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development'),
+      }
+    }),
+  ]
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 const title = 'React with Webpack and Babel with Hot Replacement';
-
+console.log(process.env.NODE_ENV);
 ReactDOM.render(
   <div>{title}</div>,
   document.getElementById('app')
