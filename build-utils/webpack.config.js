12b8e5b990b5c28628f47fe0ceaf4f11ac6b23ee
Tadeu Mendonça <tadeu.mendonca@outlook.com>
2019-10-21 22:20:43 -0300

    Working with your addons.

--- build-utils/webpack.config.js
+++ build-utils/webpack.config.js
@@ -1,6 +1,17 @@
 const webpackMerge = require('webpack-merge');
+
 const commonConfig = require('./webpack.common.js');
-module.exports = ({ env }) => {
+
+const getAddons = addonsArgs => {
+  const addons = Array.isArray(addonsArgs)
+    ? addonsArgs
+    : [addonsArgs];
+  return addons
+    .filter(Boolean)
+    .map(name => require(`./addons/webpack.${name}.js`));
+};
+
+module.exports = ({ env, addon }) => {
   const envConfig = require(`./webpack.${env}.js`);
-  return webpackMerge(commonConfig, envConfig);
+  return webpackMerge(commonConfig, envConfig, ...getAddons(addon));
 };
