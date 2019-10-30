7c0542560868a482e3341c45b1a38e270260ae94
Tadeu Mendonça <tadeu.mendonca@outlook.com>
2019-10-29 22:08:14 -0300

    Adding sass support.

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -4,15 +4,29 @@ const path = require("path");
 const {
     CleanWebpackPlugin
 } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
     module: {
         rules: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: ['babel-loader']
-        }]
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                loader: 'url-loader?limit=100000'
+            }
+        ]
     },
     resolve: {
         extensions: ['*', '.js', '.jsx']
@@ -28,7 +42,10 @@ module.exports = {
             title: 'Hello Webpack bundled JavaScript Project',
             template: './src/index.html'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        })
     ],
     devServer: {
         contentBase: './dist',
